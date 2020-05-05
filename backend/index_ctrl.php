9c0fc764562a2e4b47725faaa7c14e0f08e37ecd
theuglygram <iamerheren@yahoo.com>
2020-05-05 03:11:50 -0700

    initial commit

---
+++ backend/index_ctrl.php
@@ -0,0 +1,31 @@
+<?php 
+require "lib/SausageHTTP.php";
+
+function getCovidData() {
+    $client = new SausageHTTP\SausageHTTP\SausageHTTP();
+    $client->setRequest([
+            "URL" => 'https://api.covid19api.com/summary', 
+            "METHOD" => 'GET', 
+            "OPTIONS" => array(
+                
+            ) 
+        ]);
+    
+        return $client->response;
+}
+
+
+
+function getFlag($arg) {
+        return '<img src="https://www.countryflags.io/'.$arg.'/flat/24.png">';
+}
+
+
+
+$data =  json_decode(getCovidData());
+$countries = $data->Countries;
+$global = $data->Global;
+
+
+   
+?>
